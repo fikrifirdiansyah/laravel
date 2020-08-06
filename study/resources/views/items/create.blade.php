2897b36033e53bf6b6d88e322d7d8fd21a6254eb
fikrifirdiansyah <fikrifirdiansyah00@gmail.com>
2020-08-06 10:03:09 +0700

    Belajar template

---
+++ study/resources/views/items/create.blade.php
@@ -0,0 +1,14 @@
+@extends('master')
+
+@section('content')
+    <h2> Cvk Pusing </h2>
+@endsection
+
+@push('scripts')
+<script>
+  var wrappers = document.getElementsByClassName("wrappers");
+
+  console.log("Ini items : ", items)
+</script>
+
+@endpush
---
+++ study/resources/views/items/index.blade.php
@@ -0,0 +1,5 @@
+@extends('master')
+
+@section('content')
+    <h1> halo ini halaman index items </h1>
+@endsection
